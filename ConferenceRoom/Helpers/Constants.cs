0995d56dfc6c78654258724d9f74cd7d3b5153eb
AlmidaMuka <almida.muka@outlook.com>
2024-06-19 17:59:42 +0200

    Update .gitignore and untrack ignored files

--- ConferenceRoom/Helpers/Constants.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace ConferenceRoom.Helpers
-{
-    public static class Constants
-    {
-        public const string AdminRole = "ADMIN";
-        public const string EmployeeRole = "EMPLOYEE";
-    }
-}
